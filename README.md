4dada7a9df8a603083f517e144bd2269449a4d4d
Michael Andonie <michael@andonie.net>
2018-08-04 17:24:30 +0200

    Minor README additions Added working HelloWorld example (minimal)

--- README.md
+++ README.md
@@ -5,6 +5,11 @@ Dieses Repository enthält die Quellen zu allen **einfachen** Tutorials für die
 Die Dateien hierin sind im Regelfall funktionierende Beispiele.
 
 
+So sind die Tutorials zu benutzen
+--------------------
+
+Die Tutorials werden schrittweise erläutert auf der Engine-Alpha-Tutorialseite
+(http://engine-alpha.org/tutorials). Wer den Umgang mit der Engine lernen möchte, sollte dort starten.
 
 
 Repository-Struktur 
@@ -18,7 +23,8 @@ Ausführbarkeit
 ------
 Das Repository enthält keine IDE-Projektstrukturen oder Dependencies. 
 Daher ist der Code in diesem Repository **nicht ohne Weiteres ausführbar**.
-Wenn das Repo an sich ausgeführt werden soll, so **muss die Engine-Bibliothek in das Projekt eingebettet werden**.
+Wenn das Repo an sich ausgeführt werden soll, so **muss die Engine-Bibliothek in das Projekt eingebettet werden** 
+(sowie ggf. das Java JDK).
 
 Stattdessen können die einzelnen Beispielverzeichnisse (bzw. Beispiel-Files) auch in ein Projekt eingefügt werden,
 das bereits die Engine eingebettet hat.
--- src/tutorials/helloworld/HelloWorld.java
+++ src/tutorials/helloworld/HelloWorld.java
@@ -1,6 +1,55 @@
 package tutorials.helloworld;
 
+import ea.Game;
+import ea.Scene;
+import ea.actor.Text;
+
+/**
+ * Minimales "Hello World"-Beispiel; demonstriert die grundlegendsten Schritte zum Verwenden der Engine. Das sind:
+ * <ul>
+ *     <li>Erstellen einer Szene.</li>
+ *     <li>Erstellen eines Actors (~grafischen Objekts) und das <b>Einfügen in die Szene</b>
+ *     (notwendig, damit das Objekt sichtbar wird).</li>
+ *     <li>Einstellen des Spielfensters und der Szene in das Spielfenster.</li>
+ * </ul>
+ *
+ * <i>Erläuterungen für dieses Tutorial und weitere Tutorials sind auf
+ * <a href="http://engine-alpha.org/tutorials">unserer Tutorial-Seite</a> verfügbar.</i>
+ *
+ * @author Michael Andonie
+ */
+public class HelloWorld
+extends Scene{
+
+    /**
+     * Konstruktor. Erstellt die eine Szene, in der einfach unser "Hello World" angezeigt wird.
+     */
+    public HelloWorld() {
+        //Erstelle ein Text-Objekt (mit Inhalt "Hello World"), das angezeigt werden soll.
+        Text helloworld = new Text("Hello World");
+
+        //Füge das Text-Objekt dieser Szene hinzu.
+        //-> Erst nachdem das Objekt der Szene hinzugefügt wurde, ist es auch sichtbar.
+        this.add(helloworld);
+    }
 
 
-public class HelloWorld {
+    /**
+     * Die Main-Methode. Wird zum Starten dieses Beispiels ausgeführt.
+     * @param args Command-Line-Parameter. Vollkommen irrelevant.
+     */
+    public static void main(String[] args) {
+        //Starte einfach das Spiel:
+        startGame();
+    }
+
+    public static void startGame() {
+        //Erstelle die Szene, die angezeigt werden soll (~ unser HelloWorld)
+        Scene helloWorld = new HelloWorld();
+
+        //Startet das "Spiel" und macht damit die Szene sichtbar, die wir erstellt haben.
+        //Dabei setzen wir auch die Fenstergröße: 812 px breit, 502 px hoch.
+        //Der letzte Parameter ist die Szene, mit der das Spiel starten soll (~ unser HelloWorld)
+        Game.start(812, 592, helloWorld);
+    }
 }
